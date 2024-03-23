07c84d8f967fa4cd7f5f9cc217fdde34649b9bc5
Tomasz Szambor <szambor@gmail.com>
2024-03-23 21:51:32 +0100

    Cleanup

--- TestDoubleForLoop.java
+++
@@ -1,8 +0,0 @@
-public class TestDoubleForLoop
-{
-    public static void main(String[] args)
-    {
-        for (double x = 0.0; x <= 1.0; x+= 0.1)
-            System.out.println(x + " " + Math.sin(x));
-    }
-}
