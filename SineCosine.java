6741008336b4617b7652fe9e6bff8779f5cc2af6
Tomasz Szambor <szambor@gmail.com>
2024-03-23 21:03:02 +0100

    Cleanup

--- SineCosine.java
+++
@@ -1,9 +0,0 @@
-public class SineCosine
-{
-    public static void main(String[] args)
-    {
-        double theta = Double.parseDouble(args[0]);
-        
-        System.out.println(Math.sin(theta)*Math.sin(theta) + Math.cos(theta)*Math.cos(theta));
-    }
-}
